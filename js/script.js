5e31070251ff2aca4000ec5fce21d4a8b4df143d
Rushikesh Doke <rushikeshdoke1234@gmail.com>
2025-07-18 15:06:02 +0530

    final commit 14

--- js/script.js
+++ js/script.js
@@ -432,8 +432,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
   try {
-  const url = `https://rushikesh0864.github.io/Spotify-Frontend-/songs/${encodeURIComponent(folder)}/info.json?ts=${Date.now()}`;
-
+    const url = `https://rushikesh0864.github.io/Spotify-Frontend-/songs/${folder}/info.json?ts=${Date.now()}`;
     const response = await fetch(url);
     const data = await response.json();
 
@@ -444,8 +443,7 @@ async function getSongs(folder) {
 
     songs = data.songs.map(song => ({
       title: song,
-      path: `https://rushikesh0864.github.io/Spotify-Frontend-/songs/${encodeURIComponent(folder)}/${encodeURIComponent(song)}`
-
+      path: `https://rushikesh0864.github.io/Spotify-Frontend-/songs/${folder}/${song}`
     }));
 
     currentFolder = folder;
@@ -511,8 +509,7 @@ async function displayAlbums() {
 
   for (const folder of folders) {
     try {
-      const url = `https://rushikesh0864.github.io/Spotify-Frontend-/songs/${encodeURIComponent(folder)}/info.json?ts=${Date.now()}`;
-
+      const url = `https://rushikesh0864.github.io/Spotify-Frontend-/songs/${folder}/info.json?ts=${Date.now()}`;
       const res = await fetch(url);
       if (!res.ok) throw new Error(`Status ${res.status}`);
       const data = await res.json();
@@ -520,8 +517,8 @@ async function displayAlbums() {
       const card = document.createElement("div");
       card.className = "albumCard";
       card.innerHTML = `
-        <img src="https://rushikesh0864.github.io/Spotify-Frontend-/songs/${encodeURIComponent(folder)}/cover.jpg"
-       onerror="this.src='cover.jpg'" />
+        <img src="https://rushikesh0864.github.io/Spotify-Frontend-/songs/${folder}/cover.jpg" 
+             onerror="this.src='default.jpg'" />
         <h3>${data.title}</h3>
         <p>${data.description}</p>
         <button onclick="getSongs('${folder}')">Open</button>
